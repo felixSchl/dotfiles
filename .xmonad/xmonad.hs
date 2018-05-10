0366184858be3da6da4e4c93a531f08b11835d81
Felix Schlitter <felixschlitter@protonmail.com>
2018-05-11 03:13:59 +1200

    Set up xmonad using stack

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,25 +1,45 @@
+import Graphics.X11.ExtraTypes.XF86
+import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ScreenCorners
 import XMonad.Config.Gnome (gnomeConfig)
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
 
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ gnomeConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    , terminal = "gnome-terminal"
-    , logHook = dynamicLogWithPP xmobarPP
-        { ppOutput = hPutStrLn xmproc
-        , ppTitle = xmobarColor "green" "" . shorten 50
-        }
+    , layoutHook = avoidStruts  $ layoutHook defaultConfig
+    , handleEventHook = docksEventHook <+> handleEventHook defaultConfig
+    , startupHook = docksStartupHook <+> startupHook defaultConfig
+    , logHook =
+        dynamicLogWithPP xmobarPP
+          { ppOutput = hPutStrLn xmproc
+          , ppTitle = xmobarColor "green" "" . shorten 50
+          }
     , modMask = mod4Mask
     } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-    , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-    , ((0, xK_Print), spawn "scrot")
-    , ((mod4Mask, xK_p), spawn "dmenu_run")
+    [
+      -- Start screen saver
+      ((mod4Mask .|. shiftMask, xK_z),
+      spawn "xscreensaver-command -lock; xset dpms force off")
+
+      -- Bring up dmenu
+    , ((mod4Mask, xK_p),
+      spawn "dmenu_run")
+
+      -- Mute volume.
+    , ((0, xF86XK_AudioMute),
+      spawn "amixer -D pulse set Master 1+ toggle")
+
+    -- Decrease volume.
+    , ((0, xF86XK_AudioLowerVolume),
+      spawn "amixer -D pulse set Master 10%-")
+
+    -- Increase volume.
+    , ((0, xF86XK_AudioRaiseVolume),
+      spawn "amixer -D pulse set Master unmute && amixer -D pulse set Master 10%+")
     ]
