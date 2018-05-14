37d3e84d687575bb60618444d253b1622a01a33c
Felix Schlitter <felixschlitter@protonmail.com>
2018-05-14 23:22:40 +1200

    XMonad: Set up brightness control

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,20 +1,26 @@
+module Main where
+
 import Graphics.X11.ExtraTypes.XF86
 import System.IO
 import XMonad
+import XMonad.Actions.WindowBringer (bringMenu, gotoMenu)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ScreenCorners
-import XMonad.Config.Gnome (gnomeConfig)
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import qualified XMonad.Util.Brightness as Brightness
+import XMonad.Config.Desktop (desktopConfig)
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Actions.CycleWindows
 
+main :: IO ()
 main = do
   xmproc <- spawnPipe "xmobar"
-  xmonad $ gnomeConfig
-    { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $ layoutHook defaultConfig
-    , handleEventHook = docksEventHook <+> handleEventHook defaultConfig
-    , startupHook = docksStartupHook <+> startupHook defaultConfig
+  xmonad $ desktopConfig
+    { manageHook = manageDocks <+> manageHook def
+    , layoutHook = avoidStruts  $ layoutHook def
+    , terminal = "gnome-terminal"
+    , handleEventHook = docksEventHook <+> handleEventHook def
+    , startupHook = docksStartupHook <+> startupHook def
     , logHook =
         dynamicLogWithPP xmobarPP
           { ppOutput = hPutStrLn xmproc
@@ -27,7 +33,24 @@ main = do
       ((mod4Mask .|. shiftMask, xK_z),
       spawn "xscreensaver-command -lock; xset dpms force off")
 
-      -- Bring up dmenu
+      -- Lock to greeter
+    , ((mod4Mask .|. shiftMask, xK_l),
+      spawn "dm-tool switch-to-greeter")
+
+    , ((mod4Mask, xK_s), cycleRecentWindows [xK_Super_L] xK_s xK_w)
+    , ((mod4Mask, xK_z), rotOpposite)
+    , ((mod4Mask, xK_i), rotUnfocusedUp)
+    , ((mod4Mask, xK_u), rotUnfocusedDown)
+    , ((mod4Mask .|. controlMask, xK_i), rotFocusedUp)
+    , ((mod4Mask .|. controlMask, xK_u), rotFocusedDown)
+
+      -- Bring up dmenu to go to window
+    , ((mod4Mask, xK_g), gotoMenu)
+
+      -- Bring up dmenu to bring window here
+    , ((mod4Mask, xK_b), bringMenu)
+
+      -- Bring up dmenu to start apps
     , ((mod4Mask, xK_p),
       spawn "dmenu_run")
 
@@ -42,4 +65,14 @@ main = do
     -- Increase volume.
     , ((0, xF86XK_AudioRaiseVolume),
       spawn "amixer -D pulse set Master unmute && amixer -D pulse set Master 10%+")
+
+    -- Increase/decrease brightness
+    -- Note: Requires small manual set up on new systems (see docs)
+    , ((0, xF86XK_MonBrightnessUp), () <$ (liftIO $ Brightness.change (+50)))
+    , ((0, xF86XK_MonBrightnessDown), () <$ do
+          liftIO $ Brightness.change $ \current ->
+            if current > 100
+              then current - 50
+              else current
+      )
     ]
